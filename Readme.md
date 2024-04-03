7deae68420e95fc6ab8d96dd9e1c96e6a221300f
Mohammad <mohammad.m.m.af.1999@gmail.com>
2024-04-03 13:59:07 -0400

    Update raytracer

--- Readme.md
+++ Readme.md
@@ -1,25 +1,17 @@
-# Understandable RayTracing in 256 lines of bare C++
+# Cellular Forms Growth Simulation
 
-This repository is a support code for my computer graphics lectures. It is not meant to be the ultimate rendering code or even physically realistic. It is meant to be **simple**. This project is distributed under the [DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE](https://en.wikipedia.org/wiki/WTFPL).
+This is my implementation of the paper "Cellular Forms: an Artistic Exploration of Morphogenesis".
+The python code is used to simulate the growth of cells, and the cpp Ray Tracer is used for 3D visualization.
 
-**Check [the wiki](https://github.com/ssloy/tinyraytracer/wiki) that accompanies the source code. The second raytracing chapter is available [in the tinykaboom repository](https://github.com/ssloy/tinykaboom/wiki). If you are looking for a software rasterizer, check the [other part of the lectures](https://github.com/ssloy/tinyrenderer/wiki).**
+I included some example output videos that you can enjoy!
 
-In my lectures I tend to avoid third party libraries as long as it is reasonable, because it forces to understand what is happening under the hood. So, the raytracing 256 lines of plain C++ give us this result:
-![](https://raw.githubusercontent.com/ssloy/tinyraytracer/master/out.jpg)
 
-## compilation
+
+## Ray Tracer Compilation
 ```sh
-git clone https://github.com/ssloy/tinyraytracer.git
-cd tinyraytracer
+cd raytracer
 mkdir build
 cd build
 cmake ..
 make
 ```
-
-You can open the project in Gitpod, a free online dev evironment for GitHub:
-
-[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/ssloy/tinyraytracer)
-
-On open, the editor will compile & run the program as well as open the resulting image in the editor's preview.
-Just change the code in the editor and rerun the script (use the terminal's history) to see updated images.
--- tinyraytracer.cpp
+++ tinyraytracer.cpp
@@ -1,149 +1,214 @@
-#include <tuple>
-#include <vector>
-#include <fstream>
-#include <algorithm>
-#include <cmath>
+#define _USE_MATH_DEFINES
 
-struct vec3 {
-    float x=0, y=0, z=0;
-          float& operator[](const int i)       { return i==0 ? x : (1==i ? y : z); }
-    const float& operator[](const int i) const { return i==0 ? x : (1==i ? y : z); }
-    vec3  operator*(const float v) const { return {x*v, y*v, z*v};       }
-    float operator*(const vec3& v) const { return x*v.x + y*v.y + z*v.z; }
-    vec3  operator+(const vec3& v) const { return {x+v.x, y+v.y, z+v.z}; }
-    vec3  operator-(const vec3& v) const { return {x-v.x, y-v.y, z-v.z}; }
-    vec3  operator-()              const { return {-x, -y, -z};          }
-    float norm() const { return std::sqrt(x*x+y*y+z*z); }
-    vec3 normalized() const { return (*this)*(1.f/norm()); }
+#include <cmath>
+#include <limits>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include "geometry.h"
+
+struct Light {
+    Light(const Vec3f &p, const float i) : position(p), intensity(i) {}
+
+    Vec3f position;
+    float intensity;
 };
 
-vec3 cross(const vec3 v1, const vec3 v2) {
-    return { v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x };
-}
-
 struct Material {
-    float refractive_index = 1;
-    float albedo[4] = {2,0,0,0};
-    vec3 diffuse_color = {0,0,0};
-    float specular_exponent = 0;
+    Material(const float r, const Vec4f &a, const Vec3f &color, const float spec) : refractive_index(r), albedo(a),
+                                                                                    diffuse_color(color),
+                                                                                    specular_exponent(spec) {}
+
+    Material() : refractive_index(1), albedo(1, 0, 0, 0), diffuse_color(), specular_exponent() {}
+
+    float refractive_index;
+    Vec4f albedo;
+    Vec3f diffuse_color;
+    float specular_exponent;
 };
 
 struct Sphere {
-    vec3 center;
+    Vec3f center;
     float radius;
     Material material;
+
+    Sphere(const Vec3f &c, const float r, const Material &m) : center(c), radius(r), material(m) {}
+
+    bool ray_intersect(const Vec3f &orig, const Vec3f &dir, float &t0) const {
+        Vec3f L = center - orig;
+        float tca = L * dir;
+        float d2 = L * L - tca * tca;
+        if (d2 > radius * radius) return false;
+        float thc = sqrtf(radius * radius - d2);
+        t0 = tca - thc;
+        float t1 = tca + thc;
+        if (t0 < 0) t0 = t1;
+        if (t0 < 0) return false;
+        return true;
+    }
 };
 
-constexpr Material      ivory = {1.0, {0.9,  0.5, 0.1, 0.0}, {0.4, 0.4, 0.3},   50.};
-constexpr Material      glass = {1.5, {0.0,  0.9, 0.1, 0.8}, {0.6, 0.7, 0.8},  125.};
-constexpr Material red_rubber = {1.0, {1.4,  0.3, 0.0, 0.0}, {0.3, 0.1, 0.1},   10.};
-constexpr Material     mirror = {1.0, {0.0, 16.0, 0.8, 0.0}, {1.0, 1.0, 1.0}, 1425.};
-
-constexpr Sphere spheres[] = {
-    {{-3,    0,   -16}, 2,      ivory},
-    {{-1.0, -1.5, -12}, 2,      glass},
-    {{ 1.5, -0.5, -18}, 3, red_rubber},
-    {{ 7,    5,   -18}, 4,     mirror}
-};
-
-constexpr vec3 lights[] = {
-    {-20, 20,  20},
-    { 30, 50, -25},
-    { 30, 20,  30}
-};
-
-vec3 reflect(const vec3 &I, const vec3 &N) {
-    return I - N*2.f*(I*N);
+Vec3f reflect(const Vec3f &I, const Vec3f &N) {
+    return I - N * 2.f * (I * N);
 }
 
-vec3 refract(const vec3 &I, const vec3 &N, const float eta_t, const float eta_i=1.f) { // Snell's law
-    float cosi = - std::max(-1.f, std::min(1.f, I*N));
-    if (cosi<0) return refract(I, -N, eta_i, eta_t); // if the ray comes from the inside the object, swap the air and the media
+Vec3f refract(const Vec3f &I, const Vec3f &N, const float eta_t, const float eta_i = 1.f) { // Snell's law
+    float cosi = -std::max(-1.f, std::min(1.f, I * N));
+    if (cosi < 0)
+        return refract(I, -N, eta_i, eta_t); // if the ray comes from the inside the object, swap the air and the media
     float eta = eta_i / eta_t;
-    float k = 1 - eta*eta*(1 - cosi*cosi);
-    return k<0 ? vec3{1,0,0} : I*eta + N*(eta*cosi - std::sqrt(k)); // k<0 = total reflection, no ray to refract. I refract it anyways, this has no physical meaning
+    float k = 1 - eta * eta * (1 - cosi * cosi);
+    return k < 0 ? Vec3f(1, 0, 0) : I * eta + N * (eta * cosi -
+                                                   sqrtf(k)); // k<0 = total reflection, no ray to refract. I refract it anyways, this has no physical meaning
 }
 
-std::tuple<bool,float> ray_sphere_intersect(const vec3 &orig, const vec3 &dir, const Sphere &s) { // ret value is a pair [intersection found, distance]
-    vec3 L = s.center - orig;
-    float tca = L*dir;
-    float d2 = L*L - tca*tca;
-    if (d2 > s.radius*s.radius) return {false, 0};
-    float thc = std::sqrt(s.radius*s.radius - d2);
-    float t0 = tca-thc, t1 = tca+thc;
-    if (t0>.001) return {true, t0};  // offset the original point by .001 to avoid occlusion by the object itself
-    if (t1>.001) return {true, t1};
-    return {false, 0};
+bool scene_intersect(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &spheres, Vec3f &hit, Vec3f &N,
+                     Material &material) {
+    float spheres_dist = std::numeric_limits<float>::max();
+    for (size_t i = 0; i < spheres.size(); i++) {
+        float dist_i;
+        if (spheres[i].ray_intersect(orig, dir, dist_i) && dist_i < spheres_dist) {
+            spheres_dist = dist_i;
+            hit = orig + dir * dist_i;
+            N = (hit - spheres[i].center).normalize();
+            material = spheres[i].material;
+        }
+    }
+    return spheres_dist < 1000;
 }
 
-std::tuple<bool,vec3,vec3,Material> scene_intersect(const vec3 &orig, const vec3 &dir) {
-    vec3 pt, N;
+Vec3f
+cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &spheres, const std::vector<Light> &lights,
+         size_t depth = 0) {
+    Vec3f point, N;
     Material material;
 
-    float nearest_dist = 1e10;
-    if (std::abs(dir.y)>.001) { // intersect the ray with the checkerboard, avoid division by zero
-        float d = -(orig.y+4)/dir.y; // the checkerboard plane has equation y = -4
-        vec3 p = orig + dir*d;
-        if (d>.001 && d<nearest_dist && std::abs(p.x)<10 && p.z<-10 && p.z>-30) {
-            nearest_dist = d;
-            pt = p;
-            N = {0,1,0};
-            material.diffuse_color = (int(.5*pt.x+1000) + int(.5*pt.z)) & 1 ? vec3{.3, .3, .3} : vec3{.3, .2, .1};
+    if (depth > 1 || !scene_intersect(orig, dir, spheres, point, N, material)) {
+        return Vec3f(0.13, 0.0, 0.0); // background color
+    }
+
+    Vec3f reflect_dir = reflect(dir, N).normalize();
+    Vec3f refract_dir = refract(dir, N, material.refractive_index).normalize();
+    Vec3f reflect_orig = reflect_dir * N < 0 ? point - N * 1e-3 : point + N *
+                                                                          1e-3; // offset the original point to avoid occlusion by the object itself
+    Vec3f refract_orig = refract_dir * N < 0 ? point - N * 1e-3 : point + N * 1e-3;
+    Vec3f reflect_color = cast_ray(reflect_orig, reflect_dir, spheres, lights, depth + 1);
+    Vec3f refract_color = cast_ray(refract_orig, refract_dir, spheres, lights, depth + 1);
+
+    float diffuse_light_intensity = 0, specular_light_intensity = 0;
+    for (size_t i = 0; i < lights.size(); i++) {
+        Vec3f light_dir = (lights[i].position - point).normalize();
+        float light_distance = (lights[i].position - point).norm();
+
+        Vec3f shadow_orig = light_dir * N < 0 ? point - N * 1e-3 : point + N *
+                                                                           1e-3; // checking if the point lies in the shadow of the lights[i]
+        Vec3f shadow_pt, shadow_N;
+        Material tmpmaterial;
+        if (scene_intersect(shadow_orig, light_dir, spheres, shadow_pt, shadow_N, tmpmaterial) &&
+            (shadow_pt - shadow_orig).norm() < light_distance)
+            continue;
+
+        diffuse_light_intensity += lights[i].intensity * std::max(0.f, light_dir * N);
+        specular_light_intensity +=
+                powf(std::max(0.f, -reflect(-light_dir, N) * dir), material.specular_exponent) * lights[i].intensity;
+    }
+    return material.diffuse_color * diffuse_light_intensity * material.albedo[0] +
+           Vec3f(1., 1., 1.) * specular_light_intensity * material.albedo[1] + reflect_color * material.albedo[2] +
+           refract_color * material.albedo[3];
+}
+
+void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights, int iteration) {
+    const int width = 700;
+    const int height = 500;
+    const float fov = M_PI / 3.;
+    std::vector<Vec3f> framebuffer(width * height);
+
+#pragma omp parallel for
+    for (size_t j = 0; j < height; j++) { // actual rendering loop
+        for (size_t i = 0; i < width; i++) {
+            float dir_x = (i + 0.5) - width / 2.;
+            float dir_y = -(j + 0.5) + height / 2.;    // this flips the image at the same time
+            float dir_z = -height / (2. * tan(fov / 2.));
+            framebuffer[i + j * width] = cast_ray(Vec3f(0, 0, 0), Vec3f(dir_x, dir_y, dir_z).normalize(), spheres,
+                                                  lights);
         }
     }
 
-    for (const Sphere &s : spheres) { // intersect the ray with all spheres
-        auto [intersection, d] = ray_sphere_intersect(orig, dir, s);
-        if (!intersection || d > nearest_dist) continue;
-        nearest_dist = d;
-        pt = orig + dir*nearest_dist;
-        N = (pt - s.center).normalized();
-        material = s.material;
+    std::ofstream ofs; // save the framebuffer to file
+    std::string path = "./res/images/" + std::to_string(iteration) + ".ppm";
+    ofs.open(path, std::ios::binary);
+    ofs << "P6\n" << width << " " << height << "\n255\n";
+    for (size_t i = 0; i < height * width; ++i) {
+        Vec3f &c = framebuffer[i];
+        float max = std::max(c[0], std::max(c[1], c[2]));
+        if (max > 1) c = c * (1. / max);
+        for (size_t j = 0; j < 3; j++) {
+            ofs << (char) (255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
+        }
     }
-    return { nearest_dist<1000, pt, N, material };
+    ofs.close();
 }
 
-vec3 cast_ray(const vec3 &orig, const vec3 &dir, const int depth=0) {
-    auto [hit, point, N, material] = scene_intersect(orig, dir);
-    if (depth>4 || !hit)
-        return {0.2, 0.7, 0.8}; // background color
+double loadCoords(int iterations, int folder_name) {
+    std::ifstream ifs;
+    std::string path =
+            "./res/output/" + std::to_string(folder_name) + "/coords_" + std::to_string(iterations) +
+            ".txt";
+    ifs.open(path);
+    double scale;
+    ifs >> scale;
+    return scale;
+}
 
-    vec3 reflect_dir = reflect(dir, N).normalized();
-    vec3 refract_dir = refract(dir, N, material.refractive_index).normalized();
-    vec3 reflect_color = cast_ray(point, reflect_dir, depth + 1);
-    vec3 refract_color = cast_ray(point, refract_dir, depth + 1);
 
-    float diffuse_light_intensity = 0, specular_light_intensity = 0;
-    for (const vec3 &light : lights) { // checking if the point lies in the shadow of the light
-        vec3 light_dir = (light - point).normalized();
-        auto [hit, shadow_pt, trashnrm, trashmat] = scene_intersect(point, light_dir);
-        if (hit && (shadow_pt-point).norm() < (light-point).norm()) continue;
-        diffuse_light_intensity  += std::max(0.f, light_dir*N);
-        specular_light_intensity += std::pow(std::max(0.f, -reflect(-light_dir, N)*dir), material.specular_exponent);
+std::vector<std::vector<double>> load(int iterations, int folder_name) {
+    std::vector<std::vector<double>> res;
+    std::ifstream ifs;
+    std::string path =
+            "./res/output/" + std::to_string(folder_name) + "/" + std::to_string(iterations) + ".txt";
+    ifs.open(path);
+    while (!ifs.eof()) {
+        double x;
+        double y;
+        double z;
+        double food;
+        double color;
+        ifs >> x;
+        ifs >> y;
+        ifs >> z;
+        ifs >> food;
+        ifs >> color;
+        res.push_back({x, y, z, food, color});
     }
-    return material.diffuse_color * diffuse_light_intensity * material.albedo[0] + vec3{1., 1., 1.}*specular_light_intensity * material.albedo[1] + reflect_color*material.albedo[2] + refract_color*material.albedo[3];
+    return res;
 }
 
 int main() {
-    constexpr int   width  = 1024;
-    constexpr int   height = 768;
-    constexpr float fov    = 1.05; // 60 degrees field of view in radians
-    std::vector<vec3> framebuffer(width*height);
-#pragma omp parallel for
-    for (int pix = 0; pix<width*height; pix++) { // actual rendering loop
-        float dir_x =  (pix%width + 0.5) -  width/2.;
-        float dir_y = -(pix/width + 0.5) + height/2.; // this flips the image at the same time
-        float dir_z = -height/(2.*tan(fov/2.));
-        framebuffer[pix] = cast_ray(vec3{0,0,0}, vec3{dir_x, dir_y, dir_z}.normalized());
+    int ITERATIONS = 300;
+    int folder_name = 3;
+    //        Material ivory(1.0, Vec4f(0.6, 0.3, 0.1, 0.0), Vec3f(0.4, 0.4, 0.3), 50.);
+//        Material glass(1.5, Vec4f(0.0, 0.5, 0.1, 0.8), Vec3f(0.6, 0.7, 0.8), 125.);
+    Material red_rubber(1.0, Vec4f(0.8, 0.0, 0.0, 0.0), Vec3f(0.3, 0.1, 0.1), 20.);
+//        Material mirror(1.0, Vec4f(0.0, 10.0, 0.8, 0.0), Vec3f(1.0, 1.0, 1.0), 1425.);
+    std::vector<Light> lights;
+    lights.push_back(Light(Vec3f(-20, 20, 20), 1.5));
+    lights.push_back(Light(Vec3f(30, 50, -25), 1.8));
+    lights.push_back(Light(Vec3f(30, 20, 30), 1.7));
+    for (int i = 0; i < ITERATIONS; i++) {
+        std::vector<Sphere> spheres;
+        auto m = loadCoords(i, folder_name);
+//        std::cout << m << std::endl;
+        for (auto &c : load(i, folder_name)) {
+            spheres.push_back(
+                    Sphere(Vec3f(c[0], c[1], c[2] - 2 * m), 0.1 + c[3] / 10000.0,
+                           red_rubber));
+        }
+
+        render(spheres, lights, i);
     }
 
-    std::ofstream ofs("./out.ppm", std::ios::binary);
-    ofs << "P6\n" << width << " " << height << "\n255\n";
-    for (vec3 &color : framebuffer) {
-        float max = std::max(1.f, std::max(color[0], std::max(color[1], color[2])));
-        for (int chan : {0,1,2})
-            ofs << (char)(255 *  color[chan]/max);
-    }
+
     return 0;
 }
 
